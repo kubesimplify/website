5980626d3714224164cdb18b813624f516848452
Kunal Verma <vkunal321@gmail.com>
2022-04-26 17:40:16 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,15 +6,16 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
-  url: "https://your-docusaurus-test-site.com",
-  baseUrl: "/",
+  title: "Kubesimplify",
+  tagline: "Simplifying Cloud Native for All",
+  url: "https://github.com",
+  baseUrl: "/kubesimplify/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "kubesimplify", // Usually your GitHub org/user name.
+  projectName: "website", // Usually your repo name.
+  deploymentBranch: "gh-pages",
 
   presets: [
     [
