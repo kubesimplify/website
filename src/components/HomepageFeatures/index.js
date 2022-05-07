e3a342e18fe359d5fd991b6d8d59ee7d1bfa3123
Aayush Sharma <aayusharma1401@gmail.com>
2022-05-08 02:08:44 +0530

    structured the code

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -33,110 +33,151 @@ import Link from './assests/Link.png'
 export default function HomepageFeatures() {
 
   return (
+    
     <section className={styles.features}>
+
       <div className={styles.hp}>
-    {/* first portion(container) of the homepage */}
-      <div className={styles.hp_cont_1}>
-       <p className={styles.hp_cont_1_p1}>
-        On a mission to <br />
-        teach <span className={styles.hp_cont_1_cloud}>cloud native</span> <br />
-        to everyone.
-       </p>
-       <p className={styles.hp_cont_1_p2}>
-       We breakdown complicated concepts of <br />
-       cloud native into an easy to understand <br />
-       manner through our blogs and video content.
-       </p>
 
-       <button className={styles.hp_cont_1_btn}>
-       <p className={styles.hp_cont_1_btn_content}> 
-        Start learning
-       </p>
-      </button>
+        {/* first portion(container) of the homepage */}
+        <div className={styles.hp_cont_1}>
+
+          <p className={styles.hp_cont_1_p1}>
+            On a mission to <br />
+            teach <span className={styles.hp_cont_1_cloud}>cloud native</span> <br />
+            to everyone.
+          </p>
+
+          <p className={styles.hp_cont_1_p2}>
+            We breakdown complicated concepts of <br />
+            cloud native into an easy to understand <br />
+            manner through our blogs and video content.
+          </p>
+
+          <button className={styles.hp_cont_1_btn}>
+
+            <p className={styles.hp_cont_1_btn_content}> 
+              Start learning
+            </p>
+
+          </button>
       
-      <img className={styles.hp_cont_1_img1} src={githubActions} alt="github_actions" />
-      <img className={styles.hp_cont_1_img3} src={comments} alt="comments" />
-      <img className={styles.hp_cont_1_img2} src={computer} alt="computer" />
-      <img className={styles.hp_cont_1_img4} src={reactions} alt="reactions" />
+          <img className={styles.hp_cont_1_img1} src={githubActions} alt="github_actions" />
+          <img className={styles.hp_cont_1_img3} src={comments} alt="comments" />
+          <img className={styles.hp_cont_1_img2} src={computer} alt="computer" />
+          <img className={styles.hp_cont_1_img4} src={reactions} alt="reactions" />
 
-      </div>
+         </div>
 
-    {/* second portion(container) of the homepage */}
-    <div className={styles.hp_cont_2}>
-    <div className={styles.hp_cont_2_1}>
-      <p className={styles.hp_cont_2_p1}>Newsletter</p>
-      <p className={styles.hp_cont_2_p2}>
-      Subscribe to our <span className={styles.hp_cont_1_cloud}> newsletter </span>
-      and never miss any <br/>
-      upcoming article about <span className={styles.hp_cont_1_cloud}> cloud native </span>
-      </p>
-      </div>
-      <div className={styles.hp_cont_2_2}>
-      <div className={styles.hp_cont_2a}>
-        <p className={styles.hp_cont_2a_p}>
-          Join the mailing list :
-        </p>
-        <input className={styles.hp_cont_2a_input} type="email" placeholder="Email Address"/>
-        <button className={styles.hp_cont_2a_btn}>
-        <p className={styles.hp_cont_2a_btn_content}>
-        Subscribe 📨
-        </p>
-        </button>
-      </div>
-    </div>
-    </div>
+        {/* second portion(container) of the homepage */}
+        <div className={styles.hp_cont_2}>
 
-    {/* third portion(container) of the homepage */}
-    <div className={styles.hp_cont_3}>
-    <div className={styles.hp_cont_3_vid}>
-      <p className={styles.hp_cont_3_vid_p1}>
-      Youtube
-      </p>
-      <p className={styles.hp_cont_3_vid_p2}>
-      Video Content 
-      <img className={styles.hp_cont_3_vidIcon} src={vidIcon} alt="Video_Icon" />
-      </p>
-      <p className={styles.hp_cont_3_vid_p3}>
-      Learn cloud native technology through our videos - <br/>
-      CNCFMinutes, series, walkthroughs, livestreams and <br/>
-      many more!
-        </p>
+           <div className={styles.hp_cont_2_1}>
 
-      <a href="https://www.youtube.com/c/saiyam911" target="_blank">
-      <button className={styles.hp_cont_3_btn1}>
-       <p className={styles.hp_cont_3_btn1_content}> 
-        View Videos
-        <img className={styles.hp_cont_3_linkIcon} src={Link} alt="link" />
-       </p>
-      </button>
-    </a>
-    </div>
-    <div className={styles.hp_cont_3_blog}>
-      <p className={styles.hp_cont_3_blog_p1}>
-      Hashnode
-      </p>
-      <p className={styles.hp_cont_3_blog_p2}>
-      Blog Content 
-      <img className={styles.hp_cont_3_blogIcon} src={blogIcon} alt="Blog_Icon" />
-      </p>
-      <p className={styles.hp_cont_3_blog_p3}>
-      Learn cloud native technology through our easy to <br/>
-      understand articles where we explain everything in a <br/> 
-      simplified way!
-      </p>
+           <p className={styles.hp_cont_2_p1}>
+            Newsletter
+           </p>
 
-      <a href="https://kubesimplify.com/?t=1651944375419" target="_blank">
-      <button  className={styles.hp_cont_3_btn2}>
-       <p className={styles.hp_cont_3_btn2_content}> 
-        View Blogs 
-        <img className={styles.hp_cont_3_linkIcon} src={Link} alt="link" />
-       </p>
-      </button>
-      </a>
+            <p className={styles.hp_cont_2_p2}>
+              Subscribe to our <span className={styles.hp_cont_1_cloud}> newsletter </span>
+              and never miss any <br/>
+              upcoming article about <span className={styles.hp_cont_1_cloud}> cloud native </span>
+            </p>
 
-    </div>
-    </div>
-    </div>
+          </div>
+
+           <div className={styles.hp_cont_2_2}>
+           
+             <div className={styles.hp_cont_2a}>
+
+                <p className={styles.hp_cont_2a_p}>
+                  Join the mailing list :
+                </p>
+
+                <input className={styles.hp_cont_2a_input} type="email" placeholder="Email Address"/>
+
+                <button className={styles.hp_cont_2a_btn}>
+
+                  <p className={styles.hp_cont_2a_btn_content}>
+                    Subscribe 📨
+                  </p>
+
+                </button>
+
+               </div>
+
+             </div>
+
+          </div>
+
+        {/* third portion(container) of the homepage */}
+        <div className={styles.hp_cont_3}>
+
+         <div className={styles.hp_cont_3_vid}>
+
+            <p className={styles.hp_cont_3_vid_p1}>
+              Youtube
+            </p>
+
+            <p className={styles.hp_cont_3_vid_p2}>
+              Video Content 
+              <img className={styles.hp_cont_3_vidIcon} src={vidIcon} alt="Video_Icon" />
+            </p>
+
+            <p className={styles.hp_cont_3_vid_p3}>
+              Learn cloud native technology through our videos - <br/>
+              CNCFMinutes, series, walkthroughs, livestreams and <br/>
+              many more!
+            </p>
+
+            <a href="https://www.youtube.com/c/saiyam911" target="_blank">
+
+              <button className={styles.hp_cont_3_btn1}>
+
+                <p className={styles.hp_cont_3_btn1_content}> 
+                  View Videos
+                  <img className={styles.hp_cont_3_linkIcon} src={Link} alt="link" />
+                </p>
+
+              </button>
+            </a>
+          </div>
+
+          <div className={styles.hp_cont_3_blog}>
+            <p className={styles.hp_cont_3_blog_p1}>
+              Hashnode
+            </p>
+
+            <p className={styles.hp_cont_3_blog_p2}>
+              Blog Content 
+            <img className={styles.hp_cont_3_blogIcon} src={blogIcon} alt="Blog_Icon" />
+            </p>
+
+            <p className={styles.hp_cont_3_blog_p3}>
+              Learn cloud native technology through our easy to <br/>
+              understand articles where we explain everything in a <br/> 
+              simplified way!
+            </p>
+
+            <a href="https://kubesimplify.com/?t=1651944375419" target="_blank">
+
+              <button  className={styles.hp_cont_3_btn2}>
+                <p className={styles.hp_cont_3_btn2_content}> 
+                   View Blogs 
+                  <img className={styles.hp_cont_3_linkIcon} src={Link} alt="link" />
+                </p>
+
+              </button>
+
+            </a>
+
+          </div>
+
+        </div>
+
+       </div>
+
     </section>
+
   );
+
 }
