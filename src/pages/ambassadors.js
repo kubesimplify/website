a3943c8b7f2eb3006dd568717438ae1c120f2895
Mayank Kumar Gupta <mk6445363@gmail.com>
2022-05-09 17:38:06 +0530

    created ambassadors-hero, criteria and benifits section

---
+++ src/pages/ambassadors.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Benifits from '../components/ambassadors/Benifits'
+import Criteria from '../components/ambassadors/Criteria'
+import Hero from '../components/ambassadors/Hero'
+
+const Ambassdors = () => {
+  return (
+    <>
+      <Hero />
+      <Criteria />
+      <Benifits />
+    </>
+  )
+}
+
+export default Ambassdors
