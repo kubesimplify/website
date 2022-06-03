4a9759c980f94c2288922ab84ea8bc28318d550c
Dipankar Das <dipsonu10@hotmail.com>
2022-06-03 19:17:23 +0530

    Setup for the About Page TODO: addition of contents
    Signed-off-by: Dipankar Das <dipsonu10@hotmail.com>

---
+++ src/pages/about.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import styles from './index.module.css';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Kubesimplify`}
+      description="Description will go into a meta tag in <head />">
+      {/* <HomepageHeader /> */}
+      <main>
+        <p>Hello World!!</p>
+      </main>
+    </Layout>
+  );
+}
