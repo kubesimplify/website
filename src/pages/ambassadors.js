f63bbe9cbb674040994bfeb65890ed727a74c34d
Mayank Kumar Gupta <mk6445363@gmail.com>
2022-06-06 20:09:55 +0530

    Created FAQs section

--- src/pages/ambassadors.js
+++ src/pages/ambassadors.js
@@ -3,6 +3,7 @@ import Benifits from '../components/ambassadors/Benifits'
 import Criteria from '../components/ambassadors/Criteria'
 import Hero from '../components/ambassadors/Hero'
 import Application from '../components/ambassadors/Application'
+import FAQs from '../components/ambassadors/FAQs'
 
 const Ambassdors = () => {
   return (
@@ -11,6 +12,7 @@ const Ambassdors = () => {
       <Criteria />
       <Benifits />
       <Application />
+      <FAQs />
     </>
   )
 }
