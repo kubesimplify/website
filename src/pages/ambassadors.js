a84a05a94bd48a321e505663aeb1180f09dd39bf
Mayank Kumar Gupta <mk6445363@gmail.com>
2022-06-12 14:43:12 +0530

    Added navbar

--- src/pages/ambassadors.js
+++ src/pages/ambassadors.js
@@ -4,10 +4,12 @@ import Criteria from '../components/ambassadors/Criteria'
 import Hero from '../components/ambassadors/Hero'
 import Application from '../components/ambassadors/Application'
 import FAQs from '../components/ambassadors/FAQs'
+import Navbar from '../theme/Navbar'
 
 const Ambassdors = () => {
   return (
     <>
+      <Navbar />
       <Hero />
       <Criteria />
       <Benifits />
