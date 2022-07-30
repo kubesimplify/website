65d6e55e11e2a51f16361b9bb5b2941d465df564
Avinesh Tripathi <avineshtripathi1@gmail.com>
2022-07-30 19:38:50 +0530

    made navbar responsive

--- src/theme/Navbar/index.js
+++ src/theme/Navbar/index.js
@@ -1,92 +1,129 @@
-import React  from 'react';
+import React, { useState } from 'react';
 import styles from './styles.module.css';
+import { FaBars, FaTimes } from "react-icons/fa";
 
-
-//  Dynamic Content as it makes refactoring very easy .
 
 const navbarLogo = {
-logo: {
-  Svg: require('@site/static/img/navbarLogo.svg').default
-},
-arrow: {
-  Svg: require('@site/static/img/Arrow.svg').default
-},
+  logo: {
+    Svg: require('@site/static/img/navbarLogo.svg').default
+  },
+  arrow: {
+    Svg: require('@site/static/img/Arrow.svg').default
+  },
 }
 
-const navbarContent={
- learn: (
-  <>
+const navbarContent = {
+  learn: (
+    <>
       <div className={styles.navbar_dropdown}>
-          <button className={styles.navbar_dropbtn}>
+        <button className={styles.navbar_dropbtn}>
           Learn <navbarLogo.arrow.Svg className={styles.navbar_arrow_trying} role="img" />
-          </button>
-            <div className={styles.navbar_dropdown_content}>
-              <a href="https://www.youtube.com/c/saiyam911/videos" target="blank">Video Content</a>
-              <a className={styles.navbar_blog_design} href="https://kubesimplify.com/" target="blank">Blog Content</a>
-            </div>
-       </div>
-  </>
+        </button>
+        <div className={styles.navbar_dropdown_content}>
+          <a href="https://www.youtube.com/c/saiyam911/videos" target="blank">Video Content</a>
+          <a className={styles.navbar_blog_design} href="https://kubesimplify.com/" target="blank">Blog Content</a>
+        </div>
+      </div>
+    </>
   ),
-communityContent: (
+  communityContent: (
     <>
-    <div className={styles.navbar_dropdown}>
-          <button className={styles.navbar_dropbtn}>
+      <div className={styles.navbar_dropdown}>
+        <button className={styles.navbar_dropbtn}>
           Community <navbarLogo.arrow.Svg className={styles.navbar_arrow_trying} role="img" />
-          </button>
-               <div className={styles.navbar_dropdown_content} >
-               {/* Links for respective webpage will add when new webpages are formed */}
-
-
-                  <a  href="/website/ambassadors">Student Ambassador</a>                                                                   
-                 
-
-                                                                                  
-                                                                  
-                  
-
-                                                                                    
-
-
-                  <a className={styles.navbar_workshops_designs} href="https://kubesimplify.github.io/live-workshops/" target="blank">Workshops</a>
-
-                  <a  className={styles.navbar_contri_designs} href="#">Contributors</a>
-              </div>
-     </div>
+        </button>
+        <div className={styles.navbar_dropdown_content} >
+          {/* Links for respective webpage will add when new webpages are formed */}
+          <a href="/website/ambassadors">Student Ambassador</a>
+          <a className={styles.navbar_workshops_designs} href="https://kubesimplify.github.io/live-workshops/" target="blank">Workshops</a>
+        </div>
+      </div>
     </>
   ),
-aboutContent: (
+  aboutContent: (
     <>
-    <div className={styles.navbar_about_start}>
-      <a className={styles.navbar_about_word} href="/website/about">About</a>
-
-    </div>
+      <div className={styles.navbar_about_start}>
+        <a className={styles.navbar_about_word} href="/website/about">About</a> 
+      </div>
     </>
-    ),
+  ),
 }
- function Navbar() {
-  
+
+
+const mobileViewContent = {
+  learn : (
+    <div className={styles.mobilView_learn}>
+      <div>
+        <a href="https://www.youtube.com/c/saiyam911/videos" target="blank">Video Content</a>
+      </div>
+      <div>
+        <a className={styles.navbar_blog_design} href="https://kubesimplify.com/" target="blank">Blog Content</a>
+      </div>
+    </div>  
+  ), 
+  community : (
+    <div className={styles.mobileView_community}>
+      <div>
+      <a href="/website/ambassadors">Student Ambassador</a>
+      </div>
+      <div>
+      <a className={styles.navbar_workshops_designs} href="https://kubesimplify.github.io/live-workshops/" target="blank">Workshops</a>
+      </div>
+    </div>
+  ),
+}
+
+
+
+function Navbar() {
+
+  const [isMobile, setIsMobile] = useState(false)
+  window.addEventListener('resize', () => {
+    window.innerWidth > 680 ? setIsMobile(false) : setIsMobile(!isMobile) 
+  })
   return (
     <>
       <section className={styles.navbar}>
-          <div className={styles.navbar_positioning} >
+        <nav className={styles.navbar_positioning} >
+            <div className={styles.navbar_logopair}>
               <div className={styles.navbar_logo}>
-                   <navbarLogo.logo.Svg className={styles.logo} role="img" />
-               </div>
-               <div className={styles.navbar_about}>
+                <navbarLogo.logo.Svg className={styles.logo} role="" />
+              </div>
+              <button className={styles.mobile_menu_icon} onClick={() => setIsMobile(!isMobile)}>
+                {isMobile ? (<FaTimes />) :(<FaBars/>)}
+              </button>
+            </div>
+            <ul className={isMobile ? styles.navbar_links_mobile : styles.navbar_links} onClick={() => setIsMobile(false)}>
+              <li className={styles.about}>
                 {navbarContent.aboutContent}
-                </div>
-                <div className={styles.navbar_learn}>
-                 {navbarContent.learn}
-                </div>
-               <div className={styles.navbar_community}>
-                 {navbarContent.communityContent}
-                </div>
-                <div className={styles.navbar_contact}>
-                    <p className={styles.navbar_contact_word}> Contact </p>
-               </div>
-               <button className={styles.navbar_button}><span className={styles.navbar_text}>Newsletter</span></button>
-           </div>
-       </section>
+              </li>
+              {isMobile ? (
+                <>
+                <li>
+                  {mobileViewContent.learn}
+                </li>
+                <li >
+                  {mobileViewContent.community}
+                </li>
+                </>
+              ) : (
+                <>
+                <li className={styles.learn}>
+                  {navbarContent.learn}
+                </li>
+                <li className={styles.community}>
+                  {navbarContent.communityContent}
+                </li>
+                </>
+              )}
+              
+              <li>
+                <button className={styles.navbarbutton}><span className={styles.navbar_text}>Newsletter</span></button>
+              </li>
+            </ul>
+            
+        </nav>
+      </section>
     </>
   );
 }
