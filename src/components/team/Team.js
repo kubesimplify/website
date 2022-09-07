ce658cf18f6b231c57c2b081e944c1d9396bec3b
Your Namesanyamjain04 <sanyamjainsj04@gmail.com>
2022-09-07 19:06:27 +0530

    created a slider on the team

--- src/components/team/Team.js
+++ src/components/team/Team.js
@@ -1,44 +1,68 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "./styles.module.css";
 import { teamData } from "../../data/data";
 
+import { BsFillArrowLeftCircleFill } from "react-icons/bs";
+import { BsFillArrowRightCircleFill } from "react-icons/bs";
 import { TiSocialLinkedin } from "react-icons/ti";
 import { AiFillGithub } from "react-icons/ai";
 import { AiOutlineTwitter } from "react-icons/ai";
 
 const Team = () => {
-  console.log(teamData);
+  const card = useRef();
+  const prevCard = () => {
+    let width = card.current.clientWidth;
+    let change = width > 835 ? 402 : 175;
+    card.current.scrollLeft -= change;
+  };
+  const nextCard = () => {
+    let width = card.current.clientWidth;
+    let change = width > 835 ? 402 : 175;
+    card.current.scrollLeft += change;
+  };
+
   return (
     <div className={styles.team}>
       <h1 className={styles.heading}>Team</h1>
-
-      <div className={styles.cards}>
-        {teamData.map((user, index) => (
-          <div key={index} className={styles.card}>
-            <img className={styles.coverPhoto} src={user.coverPhoto} />
-            <div className={styles.info}>
-              <h3 className={styles.name}>{user.name}</h3>
-              <h4 className={styles.role}>{user.role}</h4>
-              <div className={styles.socials}>
-                <span>
-                  <a href={user.twitterLink}>
-                    <AiOutlineTwitter className={styles.social} />
-                  </a>
-                </span>
-                <span>
-                  <a href={user.linkedinLink}>
-                    <TiSocialLinkedin className={styles.social} />
-                  </a>
-                </span>
-                <span>
-                  <a href={user.githubLink}>
-                    <AiFillGithub className={styles.social} />
-                  </a>
-                </span>
+      <div className={styles.slider}>
+        <span className={styles.previous_btn} onClick={() => prevCard()}>
+          <BsFillArrowLeftCircleFill size={25} />
+        </span>
+        <div ref={card} className={styles.cards}>
+          {teamData.map((user, index) => (
+            <div key={index} className={styles.card}>
+              <img
+                className={styles.coverPhoto}
+                src={user.coverPhoto}
+                draggable="false"
+              />
+              <div className={styles.info}>
+                <h3 className={styles.name}>{user.name}</h3>
+                <h4 className={styles.role}>{user.role}</h4>
+                <div className={styles.socials}>
+                  <span>
+                    <a href={user.twitterLink}>
+                      <AiOutlineTwitter className={styles.social} />
+                    </a>
+                  </span>
+                  <span>
+                    <a href={user.linkedinLink}>
+                      <TiSocialLinkedin className={styles.social} />
+                    </a>
+                  </span>
+                  <span>
+                    <a href={user.githubLink}>
+                      <AiFillGithub className={styles.social} />
+                    </a>
+                  </span>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        <span className={styles.next_btn} onClick={() => nextCard()}>
+          <BsFillArrowRightCircleFill size={25} />
+        </span>
       </div>
     </div>
   );
