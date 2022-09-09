e28f8a3ac9c251f3484d4f77ca5c8411f598b77b
Your Namesanyamjain04 <sanyamjainsj04@gmail.com>
2022-09-09 16:34:26 +0530

    recreated ambasssador page

--- src/components/ambassadors/Benifits.js
+++ src/components/ambassadors/Benifits.js
@@ -1,72 +1,88 @@
-import React from 'react'
-import Styles from './style.module.css'
+import React from "react";
+import styles from "./style.module.css";
 
-import swags from './img/swags.png'
-import promotion from './img/promotion.png'
-import mentoring from './img/mentoring.png'
-import discord from './img/discord.png'
-import opportunities from './img/opportunities.png'
-import learning from './img/learning.png'
-
+import swags from "./img/swags.png";
+import promotion from "./img/promotion.png";
+import mentoring from "./img/mentoring.png";
+import discord from "./img/discord.png";
+import opportunities from "./img/opportunities.png";
+import qrcode from "./img/qr_code.png";
+import learning from "./img/learning.png";
+import ambassdors from "../HomepageFeatures/assests/Ambassadors.png";
 
 const benifits = [
-    {
-        'logo': swags,
-        'title': 'Swags',
-        'para': "You'll get amazing swags and goodies"
-    },
-    {
-        'logo': promotion,
-        'title': 'Promotion',
-        'para': "Get visibility in the community whenever you contribute"
-    },
-    {
-        'logo': discord,
-        'title': 'Private Discord',
-        'para': "Get access to private discord channel with other team members"
-    },
-    {
-        'logo': mentoring,
-        'title': 'Mentoring',
-        'para': "Monthly coaching/mentorship session with Saiyam Pathak"
-    },
-    {
-        'logo': opportunities,
-        'title': 'Opportunities',
-        'para': "Get notified if there are any internships or job opportunities"
-    },
-    {
-        'logo': learning,
-        'title': 'Learning',
-        'para': "Lots of mini projects & workshops to get you the real experience"
-    }
-]
-
+  {
+    logo: swags,
+    title: "Swags",
+    para: "You'll get amazing swags and goodies",
+  },
+  {
+    logo: promotion,
+    title: "Promotion",
+    para: "Get visibility in the community whenever you contribute",
+  },
+  {
+    logo: discord,
+    title: "Private Discord",
+    para: "Get access to private discord channel with other team members",
+  },
+  {
+    logo: mentoring,
+    title: "Mentoring",
+    para: "Monthly coaching/mentorship session with Saiyam Pathak",
+  },
+  {
+    logo: opportunities,
+    title: "Opportunities",
+    para: "Get notified if there are any internships or job opportunities",
+  },
+  {
+    logo: learning,
+    title: "Learning",
+    para: "Lots of mini projects & workshops to get you the real experience",
+  },
+];
 
 const Benifits = () => {
-    return (
-        <div className={Styles.benifits_containner}>
-            <h2 className={Styles.benifits_title}>Benefits</h2>
-            <div className={Styles.benifits_underline}></div>
+  return (
+    <div className={styles.benifits_containner}>
+      <h2 className={styles.benifits_title}>Benefits</h2>
+      {/* <div className={styles.benifits_underline}></div> */}
 
-            <div className={Styles.benifits_flex}>
+      <div className={styles.benifits_flex}>
+        {benifits.map((benifit, index) => (
+          <div className={styles.benifits_items} key={index} >
+            <img
+              className={styles.benifits_items_img}
+              src={benifit.logo}
+              alt="img not found"
+            />
+            <h2 className={styles.benifits_items_heading}>{benifit.title}</h2>
+            <p className={styles.benifits_items_para}>{benifit.para}</p>
+          </div>
+        ))}
+      </div>
 
-                {benifits.map(function (benifit) {
-                    return (
-                        <div className={Styles.benifits_items}>
-                            <img className={Styles.benifits_items_img} src={benifit.logo} alt="img not found" />
-                            <h2 className={Styles.benifits_items_heading}>{benifit.title}</h2>
-                            <p className={Styles.benifits_items_para}>{benifit.para}</p>
-                        </div>
-
-                    )
-                })}
-
-            </div>
+      <div className={styles.ambassador}>
+        <div className={styles.application}>
+          <div>
+            <h1>Application</h1>
+            <p>
+              Apply for the Kubesimplify Ambassador Program now if you want to
+              be a part of the mission!
+            </p>
+            <button className={styles.application_btn}>
+              <a href="https://docs.google.com/forms/d/e/1FAIpQLSfsiMZoKAC4eulEDNVnTOaujoortij2e9goZv355N_nTdjCjA/viewform" target="_blank">Apply Now</a>
+            </button>
+          </div>
+          <img src={qrcode} alt="image" height={175} width={175} />
         </div>
+        <div className={styles.benefits_img}>
+          <img className={styles.ambassador_img} src={ambassdors} alt="Blog" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-    )
-}
-
-
-export default Benifits
+export default Benifits;
--- src/components/ambassadors/FAQs.js
+++ src/components/ambassadors/FAQs.js
@@ -1,40 +1,42 @@
-import React from 'react'
-import Styles from './style.module.css'
+import React from "react";
+import Styles from "./style.module.css";
 
-const faqs=[
-    {
-        'question': 'Q. How do I define being a Kubesimplify Ambassador?',
-        'answer': 'You being a Kubesimplify Ambassador should be referred to as “Volunteer Experience” and not as “Employment” or “Work Experience”'
-    },
-    {
-        'question': 'Q. Can I be removed as a Kubesimplify Ambassador?',
-        'answer': 'Kubesimplify will provide you with the support and other perks but if you are not active in the community or do not participate regulary, you will be removed as an Ambassador'
-    },
-    {
-        'question': 'Q. Can I use the logo of Kubesimplify to represent myself as an Ambassador?',
-        'answer': 'You cannot use the logo as your profile picture but you can use it if you are mentioning yourself as a Kubesimplify Ambassador'
-    }
-]
+const faqs = [
+  {
+    question: "Q. How do I define being a Kubesimplify Ambassador?",
+    answer:
+      "You being a Kubesimplify Ambassador should be referred to as “Volunteer Experience” and not as “Employment” or “Work Experience”.",
+  },
+  {
+    question: "Q. Can I be removed as a Kubesimplify Ambassador?",
+    answer:
+      "Kubesimplify will provide you with the support and other perks but if you are not active in the community or do not participate regulary, you will be removed as an Ambassador.",
+  },
+  {
+    question:
+      "Q. Can I use the logo of Kubesimplify to represent myself as an Ambassador?",
+    answer:
+      "You cannot use the logo as your profile picture but you can use it if you are mentioning yourself as a Kubesimplify Ambassador.",
+  },
+];
 
 const FAQs = () => {
   return (
     <div className={Styles.FAQs_containner}>
+      <div className={Styles.faq_section}>
         <h2 className={Styles.FAQs_title}>FAQs</h2>
-        <div className={Styles.FAQs_underline}></div>
 
         <div className={Styles.FAQs_flex}>
-            {faqs.map(function(faq){
-                return(
-                    <>
-                    <p className={Styles.FAQs_question}>{faq.question} </p>
-                    <p className={Styles.FAQs_answer}>{faq.answer}</p>
-                    </>
-                )
-            })}
+          {faqs.map((faq, index) => (
+            <div key={index}>
+              <p className={Styles.FAQs_question}>{faq.question} </p>
+              <p className={Styles.FAQs_answer}>{faq.answer}</p>
+            </div>
+          ))}
         </div>
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FAQs
+export default FAQs;
