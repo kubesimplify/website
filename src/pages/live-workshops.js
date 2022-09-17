fcf4801b1105df3055c035e09efbf699f1e5233f
Sanyam Jain <sanyamjainsj04@gmail.com>
2022-09-17 11:28:50 +0530

    created live-workshops page in react (#185)
    * created live- workshop page in react
    
    * fixed typo

---
+++ src/pages/live-workshops.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LiveWorkshop from '../components/liveworkshop/LiveWorkshop'
+
+const LiveWorkshops = () => {
+  return (
+    <>
+        <LiveWorkshop />
+    </>
+  )
+}
+
+export default LiveWorkshops
